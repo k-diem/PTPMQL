8191abd0878c8b888c7612941847a80bf534aa20
k-diem <duongkhanhdiem2907@gmail.com>
2023-09-07 17:46:33 +0700

    commit note

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using NewApp.Models;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Khoi tao 2 doi tuong thu class Person
+        Person ps1 = new Person();
+        Person ps2 = new Person();
+        //Gan gia tri cho thuoc tinh doi tuong ps1
+        ps1.FullName = "Nguyen Van A";
+        ps1.Address = "Ha Noi";
+        ps1.Age = 18;
+        //gan gia tri cho thuoc tinh doi tuong ps2
+        ps2.FullName = "Ho ten mac dinh";
+        ps2.Address = "Dia chi mac dinh";
+        ps2.Age = 20;
+        //goi phuong thuc hien thi thong tin
+        ps1.Display();
+        ps2.Display();
+    }
+}
