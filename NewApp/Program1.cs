828997e375f0473f49dd8f2640724e1a410103c2
k-diem <duongkhanhdiem2907@gmail.com>
2023-09-07 17:55:26 +0700

    commit note

---
+++ NewApp/Program1.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Nhap vao du lieu: ");
+        string str = Console.ReadLine();
+        Console.WriteLine("Du lieu vua nhap vao la: " + str);
+    }
+}
