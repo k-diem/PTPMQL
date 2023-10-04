6e05b4adc02d78fc7e4dc0e11f75ba7d9ef00b48
k-diem <duongkhanhdiem2907@gmail.com>
2023-10-04 23:56:02 +0700

    commit note

--- NewApp/Models/Person.cs
+++ NewApp/Models/Person.cs
@@ -1,31 +1,49 @@
+// using System;
+// using System.Security.AccessControl;
+// namespace NewApp.Models
+// {
+//     public class Person
+//     {
+//         public string FullName { get; set; }
+//         public string Address { get; set; }
+//         public int Age { get; set; }
+//         public void EnterData()
+//         {
+//             System.Console.Write("Full Name = ");
+//             FullName = Console.ReadLine();
+//             System.Console.Write("Address = ");
+//             Address = Console.ReadLine();
+//             System.Console.Write("Age = ");
+//             try{
+//                 //cau lenh co the xay ra ngoai le
+//                 Age = Convert.ToInt16(Console.ReadLine());
+//             }catch(Exception e)
+//             {
+//                 //cau lenh xu ly ngoai le
+//                 Age = 0;
+//             }
+//         }
+//         public void Display()
+//         {
+//             System.Console.WriteLine("{0} - {1} - {2} tuoi", FullName, Address, Age);
+//         }
+//     }
+// }
 using System;
 using System.Security.AccessControl;
-namespace NewApp.Models
+namespace NewApp.Models{
+public class Person
 {
-    public class Person
+    public int PersonID { get; set; }
+    public string FullName { get; set; }
+
+    public void NhapThongTin()
     {
-        public string FullName { get; set; }
-        public string Address { get; set; }
-        public int Age { get; set; }
-        public void EnterData()
-        {
-            System.Console.Write("Full Name = ");
-            FullName = Console.ReadLine();
-            System.Console.Write("Address = ");
-            Address = Console.ReadLine();
-            System.Console.Write("Age = ");
-            try{
-                //cau lenh co the xay ra ngoai le
-                Age = Convert.ToInt16(Console.ReadLine());
-            }catch(Exception e)
-            {
-                //cau lenh xu ly ngoai le
-                Age = 0;
-            }
-        }
-        public void Display()
-        {
-            System.Console.WriteLine("{0} - {1} - {2} tuoi", FullName, Address, Age);
-        }
+        Console.WriteLine("ID: ");
+        PersonID = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Full Name: ");
+        FullName = Console.ReadLine();
     }
+}
 }
