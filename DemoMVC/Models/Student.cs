cf14e3a39502e522eccc93235691086b73ebc9fe
k-diem <duongkhanhdiem2907@gmail.com>
2023-10-21 10:06:26 +0700

    StudentController

---
+++ DemoMVC/Models/Student.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Security.AccessControl;
+
+namespace DemoMVC.Models
+{
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string FullName { get; set; }
+    }
+}
