ee8954c5cd1907417e5e99a5f102af4bc1ae93c3
k-diem <duongkhanhdiem2907@gmail.com>
2023-11-01 21:04:18 +0700

    Slide 68 - 6_C# OOP

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,50 +1,5 @@
 ﻿// //DUONG KHANH DIEM - 2021050130
-// using System;
-// using System.Collections;
-// namespace NewApp.Models
-// {
-//     class Program
-//     {
-//         static void Main(string[] args)
-//         {
-//             // Tạo danh sách các đối tượng Person
-//             var psList = new List<Person>();
-
-//             for (int i = 0; i < 2; i++)
-//             {
-//                 Person ps = new Person();
-//                 ps.NhapThongTin();
-//                 psList.Add(ps);
-//             }
-
-//             foreach (Person p in psList)
-//             {
-//                 Console.WriteLine(p.PersonID + "-" + p.FullName);
-//             }
-//             //them mot doi tuong ps vao danh sach
-//             for (int i = 0; i < 2; i++)
-//             {
-//                 Person ps = new Person();
-//                 ps.NhapThongTin();
-//                 psList.Add(ps);
-//             }
-//             //sua mot doi tuong trong danh sach
-//             int indexToEdit = 0; // chi muc cua phan tu duoc sua doi
-//             psList[indexToEdit].NhapThongTin(); // sua doi thong tin cua phan tu
-//             //xoa mot phan tu trong danh sach
-//             int indexToRemove = 1; // chi muc can xoa
-//             psList.RemoveAt(indexToRemove); // xoa phan tu chi muc da chon
-
-
-
-//         }
-//     }
-
-
-// }
-
-
-//CLASS STUDENT KE THUA TU CLASS PERSON
+//Thực hành ArrayList
 using System;
 using System.Collections;
 namespace NewApp.Models
@@ -53,14 +8,60 @@ namespace NewApp.Models
     {
         static void Main(string[] args)
         {
-            Student student = new Student();
-            student.NhapThongTinStudent();
+            // Tạo danh sách các đối tượng Person
+            var psList = new List<Person>();
 
-            Console.WriteLine("Information of student:");
-            Console.WriteLine($"ID: {student.PersonID}");
-            Console.WriteLine($"Full Name: {student.FullName}");
-            Console.WriteLine($"Student ID: {student.StudentID}");
-            Console.WriteLine($"Major: {student.Major}");
+            for (int i = 0; i < 2; i++)
+            {
+                Person ps = new Person();
+                ps.NhapThongTin();
+                psList.Add(ps);
+            }
+
+            foreach (Person p in psList)
+            {
+                Console.WriteLine(p.PersonID + "-" + p.FullName);
+            }
+            //them mot doi tuong ps vao danh sach
+            for (int i = 0; i < 2; i++)
+            {
+                Person ps = new Person();
+                ps.NhapThongTin();
+                psList.Add(ps);
+            }
+            //sua mot doi tuong trong danh sach
+            int indexToEdit = 0; // chi muc cua phan tu duoc sua doi
+            psList[indexToEdit].NhapThongTin(); // sua doi thong tin cua phan tu
+            //xoa mot phan tu trong danh sach
+            int indexToRemove = 1; // chi muc can xoa
+            psList.RemoveAt(indexToRemove); // xoa phan tu chi muc da chon
+
+
+
         }
     }
+
+
 }
+
+
+// //CLASS STUDENT KE THUA TU CLASS PERSON
+// using System;
+// using System.Collections;
+// namespace NewApp.Models
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             Student student = new Student();
+//             student.NhapThongTinStudent();
+
+//             Console.WriteLine("Information of student:");
+//             Console.WriteLine($"ID: {student.PersonID}");
+//             Console.WriteLine($"Full Name: {student.FullName}");
+//             Console.WriteLine($"Student ID: {student.StudentID}");
+//             Console.WriteLine($"Major: {student.Major}");
+//         }
+//     }
+// }
