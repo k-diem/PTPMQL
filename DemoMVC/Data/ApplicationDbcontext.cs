5d7775460bd893f71bbbc669fc96f7cafbbdac0d
k-diem <duongkhanhdiem2907@gmail.com>
2023-11-09 01:12:49 +0700

    Xây dựng Controller và View cho class Employee

--- DemoMVC/Data/ApplicationDbcontext.cs
+++ DemoMVC/Data/ApplicationDbcontext.cs
@@ -6,5 +6,6 @@ namespace DemoMVC.Data{
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {}
         public DbSet<Person> Person{ get; set;}  
+        public DbSet<DemoMVC.Models.Employee> Employee { get; set; } = default!;
     }
 }
