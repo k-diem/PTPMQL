266b8f37b63db58c0708d2949ac8eafef6e6e580
k-diem <duongkhanhdiem2907@gmail.com>
2023-11-09 01:23:36 +0700

    Sinh code tu dong DaiLy

--- DemoMVC/Data/ApplicationDbcontext.cs
+++ DemoMVC/Data/ApplicationDbcontext.cs
@@ -7,5 +7,6 @@ namespace DemoMVC.Data{
         {}
         public DbSet<Person> Person{ get; set;}  
         public DbSet<DemoMVC.Models.Employee> Employee { get; set; } = default!;
+        public DbSet<DemoMVC.Models.DaiLy> DaiLy { get; set; } = default!;
     }
 }
